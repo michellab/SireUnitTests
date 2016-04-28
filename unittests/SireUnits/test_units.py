c2a99c8197dacb474915ac1689c16530cb5450d7
Christopher Woods <chryswoods@gmail.com>
2016-04-28 11:03:24 +0100

    Finished cleaning up all of the unit tests

---
+++ unittests/SireUnits/test_units.py
@@ -0,0 +1,46 @@
+
+from Sire.Units import *
+from Sire.Move import *
+
+from Sire.Maths import pi
+
+def _pvt_close( val0, val1 ):
+   
+   if not (abs(float(val1)-float(val0)) < 0.000001):
+        print("FAIL! %f != %f" % (val0, val1))
+        return False
+   else:
+        return True
+
+def test_units(verbose=False):
+    temp = convert(100, fahrenheit, celsius)
+
+    if verbose:   
+        print("100 F == %f C" % (100*fahrenheit).to(celsius))
+
+    assert( _pvt_close(temp, (100-32)/1.8) )
+    assert( _pvt_close(temp, (100*fahrenheit).to(celsius)) )
+
+    mc = RigidBodyMC()
+
+    mc.setTemperature( 100 * fahrenheit )
+
+    if verbose:
+        print(mc.temperature().to(celsius))
+
+    assert( _pvt_close(mc.temperature().to(fahrenheit), 100) )
+
+    k = (4 * pi * 8.854187817e-12 * farad / meter)
+
+    if verbose:
+        print(k)
+
+    k = 1 / k
+
+    if verbose:
+        print(k)
+
+    assert( _pvt_close(k.value(), 332.063710) )
+
+if __name__ == "__main__":
+    test_units(True)
---
+++ unittests/SireVol/test_arrays.py
@@ -0,0 +1,74 @@
+
+from Sire.Vol import *
+from Sire.IO import *
+
+import copy
+
+def test_arrays(verbose=False):
+
+    coords = PDB().readMolecule("../io/sb2.pdb") \
+                  .molecule().property("coordinates").array()
+
+    coords2 = copy.copy(coords)
+
+    if verbose:
+        print((coords == coords2))
+
+        print(coords.nCoordGroups(), coords.nCoords())
+
+    coords.remove(1,2)
+
+    assert( coords.nCoordGroups() == coords2.nCoordGroups()-2 )
+    assert( coords.nCoords() == coords2.nCoords() - 
+                                coords2[1].count() - coords2[2].count() )
+
+    if verbose:
+        print(coords.nCoordGroups(), coords.nCoords())
+        print(coords2.nCoordGroups(), coords2.nCoords())
+
+        print((coords == coords2))
+
+    ngroups = coords.nCoordGroups()
+    ncoords = coords.nCoords()
+
+    coords.append(coords2)
+
+    assert( coords.nCoordGroups() == ngroups + coords2.nCoordGroups() )
+    assert( coords.nCoords() == ncoords + coords2.nCoords() )
+
+    if verbose:
+        print(coords.nCoordGroups(), coords.nCoords())
+
+    while coords.nCoordGroups() != coords2.nCoordGroups():
+        coords.remove(0)
+
+    assert( coords == coords2 )
+
+    if verbose:
+        print(coords.nCoordGroups(), coords.nCoords())
+
+    coords.append(coords)
+    coords.append(coords)
+
+    assert( coords.nCoordGroups() == coords2.nCoordGroups() * 4 )
+    assert( coords.nCoords() == coords2.nCoords() * 4 )
+
+    for i in range(0,coords2.nCoordGroups()):
+        assert( coords[i] == coords[i+coords2.nCoordGroups()] )
+        assert( coords[i] == coords[i+2*coords2.nCoordGroups()] )
+        assert( coords[i] == coords[i+3*coords2.nCoordGroups()] )
+
+    if verbose:
+        print(coords.nCoordGroups(), coords.nCoords())
+
+    coords.remove(0, coords.nCoordGroups())
+
+    if verbose:
+        print(coords.nCoordGroups(), coords.nCoords())
+
+    assert( coords.isEmpty() )
+
+if __name__ == "__main__":
+    test_arrays(True)
+
+

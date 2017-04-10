77b8a9b57f55ce50159c05c55633410f944ea713
Christopher Woods <chryswoods@gmail.com>
2017-04-10 21:12:20 +0100

    Added in tests for parsing dictionaries

--- unittests/Tools/test_parser.py
+++ unittests/Tools/test_parser.py
@@ -1,6 +1,13 @@
 
-from Sire.Tools import resolveParameters,readParams
+from Sire.Tools import resolveParameters,readParams,Parameter
 from Sire.Units import *
+
+distance_restraints_dict = Parameter("distance restraints dictionary", {},
+                                     """Dictionary of pair of atoms whose distance is restrained, and restraint
+                                     parameters. Syntax is {(atom0,atom1):(reql, kl, Dl)} where atom0, atom1 are atomic
+                                     indices. reql the equilibrium distance. Kl the force constant of the restraint.
+                                     D the flat bottom radius. WARNING: PBC distance checks not implemented, avoid
+                                     restraining pair of atoms that may diffuse out of the box.""")
 
 params = readParams("configfile")
 
@@ -10,7 +17,13 @@ params["test3"] = 500
 
 @resolveParameters
 def test_resolveparams():
-    print("Hello")
+    
+    print(params)
+
+    # this should work
+    dic_items = list(distance_restraints_dict.val.items())
+
+    print(dic_items)
 
 if __name__ == "__main__":
     test_resolveparams(params)

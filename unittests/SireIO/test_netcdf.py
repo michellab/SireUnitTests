b12fd9c57922464b70044106bab1747cf2f21293
Christopher Woods <chryswoods@gmail.com>
2017-07-20 12:34:47 +0100

    Small changes to the netcdf test

--- unittests/SireIO/test_netcdf.py
+++ unittests/SireIO/test_netcdf.py
@@ -3,9 +3,15 @@ from Sire.IO import *
 
 def test_netcdf(verbose=False):
 
-    #r = AmberRst("test.rst7")
     r = AmberRst("../io/NA16.rst")
 
+    print(r)
+
+    print(r.creatorApplication())
+
+    s = MoleculeParser.read("../io/NA16.rst", "../io/NA16.top")
+
+    PDB().write(s.molecules(), "test.pdb")
 
 if __name__ == "__main__":
     test_netcdf(True)

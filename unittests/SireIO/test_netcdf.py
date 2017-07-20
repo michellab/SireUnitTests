14d8bceaa700072c78cc2a4b149a38d6fe1d56f6
Christopher Woods <chryswoods@gmail.com>
2017-07-20 11:16:54 +0100

    Added in tests for the NetCDF code

---
+++ unittests/SireIO/test_netcdf.py
@@ -0,0 +1,12 @@
+
+from Sire.IO import *
+
+def test_netcdf(verbose=False):
+
+    #r = AmberRst("test.rst7")
+    r = AmberRst("../io/NA16.rst")
+
+
+if __name__ == "__main__":
+    test_netcdf(True)
+

7cb6d766722c2b46a4ac340c99586de379cecf20
Christopher Woods <chryswoods@gmail.com>
2017-08-10 17:38:35 +0100

    Added more tests for the Gro87 parser. These fail because the velocity data is not consistent...

--- unittests/SireIO/test_gro87.py
+++ unittests/SireIO/test_gro87.py
@@ -13,15 +13,97 @@ except:
     # No Gro87 support
     has_gro87 = False
 
+def _assert_equal( x, y, diff ):
+    if abs(x-y) > diff:
+        assert_equal(x, y)
+
+def _assert_almost_equal( v0, v1 ):
+    assert_equal( len(v0), len(v1) )
+
+    is_vec = False
+
+    try:
+        v0[0].x()
+        is_vec = True
+    except:
+        pass
+
+    if is_vec:
+        for i in range(0,len(v0)):
+            _assert_equal( v0[i].x(), v1[i].x(), 0.01 )
+            _assert_equal( v0[i].y(), v1[i].y(), 0.01 )
+            _assert_equal( v0[i].z(), v1[i].z(), 0.01 )
+    else:
+        for i in range(0,len(v0)):
+            _assert_equal( v0[i], v1[i], 0.01 )
+
+def test_gro_rst(verbose=False):
+    if not has_gro87:
+        return
+
+    # read in the system using a different format
+    if verbose:
+        print("Reading in rst/top file...")
+
+    s = MoleculeParser.read("../io/NA16.rst", "../io/NA16.top")
+
+    # Convert the data to Gro87
+    if verbose:
+        print("Converting...")
+
+    g = Gro87(s)
+
+    # Ensure that the numbers of atoms etc. are all correct
+    if verbose:
+        print("Converting to RST...")
+
+    a = AmberRst(s)
+
+    if verbose:
+        print("Comparing...")
+
+    assert_equal( g.nAtoms(), a.nAtoms() )
+    assert_equal( g.hasVelocities(), a.hasVelocities() )
+
+    _assert_almost_equal( g.coordinates(), a.coordinates() )
+    _assert_almost_equal( g.velocities(), a.velocities() )
+
+    if verbose:
+        print("Writing...")
+
+    g.writeToFile("test.gro")
+
+    if verbose:
+        print("Re-reading...")
+
+    g2 = Gro87("test.gro")
+
+    if verbose:
+        print("Comparing...")
+
+    assert_equal( g.nAtoms(), g2.nAtoms() )
+    assert_equal( g.hasVelocities(), g2.hasVelocities() )
+
+    _assert_almost_equal( g.coordinates(), g2.coordinates() )
+    _assert_almost_equal( g.velocities(), g2.velocities() )
+    _assert_almost_equal( g.atomNumbers(), g2.atomNumbers() )
+    _assert_almost_equal( g.atomNames(), g2.atomNames() )
+    _assert_almost_equal( g.residueNumbers(), g2.residueNumbers() )
+    _assert_almost_equal( g.residueNames(), g2.residueNames() )
+
+    assert_equal( g.boxV1(), g2.boxV1() )
+    assert_equal( g.boxV2(), g2.boxV2() )
+    assert_equal( g.boxV3(), g2.boxV3() )
+
 def test_gro87(verbose=False):
     if not has_gro87:
         return
 
-    grofile = "../io/water.gro"
-    #grofile = "../io/urea.gro"
-
     if verbose:
         print("Reading the Gro87 file using the Gro87 parser...")
+
+    grofile = "../io/water.gro"
+    #grofile = "../io/urea.gro"
 
     gro87 = Gro87(grofile)
 
@@ -30,3 +112,4 @@ def test_gro87(verbose=False):
 
 if __name__ == "__main__":
     test_gro87(True)
+    test_gro_rst(True)

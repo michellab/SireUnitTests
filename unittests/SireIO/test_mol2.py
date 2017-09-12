8853c36780fb69f55114730f742e2d9811bb81e0
Lester Hedges <lester.hedges@gmail.com>
2017-09-12 20:48:59 +0100

    Initial test file for Mol2 parser.

--- unittests/SireIO/test_mol2.py
+++ unittests/SireIO/test_mol2.py
@@ -1,4 +1,3 @@
-
 from Sire.IO import *
 from Sire.Mol import *
 
@@ -17,29 +16,9 @@ def test_mol2(verbose=False):
     if not has_mol2:
         return
 
-    pdbfile = "../io/dioxin.pdb"
     mol2file = "../io/dioxin.mol2"
 
-    if verbose:
-        print("Reading the PDB file using the PDB parser...")
-
-    pdbmols = MoleculeParser.read(pdbfile)
-
-    if verbose:
-        print("Reading the Mol2 file using the Mol2 parser...")
-
-    mol2mols = MoleculeParser.read(mol2file)
-
-    # make sure the same number of molecules have been written
-    assert_equal( pdbmols.nMolecules(), mol2mols.nMolecules() )
-
-    # now compare the first molecule loaded from the file by both parsers
-    pdbmol = oldmols[MolIdx(0)]
-    mol2mol = newmols[MolIdx(0)]
-
-    # write some tests that compare these two proteins
-    assert_equal( pdbmol.nAtoms(), mol2mol.nAtoms() )
-
+    m = Mol2(mol2file);
 
 if __name__ == "__main__":
     test_mol2(True)

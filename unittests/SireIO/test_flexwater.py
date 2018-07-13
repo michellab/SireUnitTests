ecf304c5f88854bf3dbfe75d5881fe318b528056
Christopher Woods <chryswoods@gmail.com>
2018-07-13 18:59:29 +0100

    Added in tests for fixes for reading flexible waters from gromacs files and tests that show that spacewrapping works for multi-timestep simulations

---
+++ unittests/SireIO/test_flexwater.py
@@ -0,0 +1,73 @@
+
+from Sire.IO import *
+from Sire.MM import *
+from Sire.Mol import *
+from Sire.CAS import *
+from Sire.Units import *
+
+from nose.tools import assert_equal, assert_almost_equal
+
+OW = AtomName("OW")
+HW1 = AtomName("HW1")
+HW2 = AtomName("HW2")
+
+k_oh = 0.5 * 502416.0 * (kJ_per_mol / (nanometer*nanometer))
+r_oh = 0.09572 * nanometer
+
+k_hoh = 0.5 * 628.02 * (kJ_per_mol / (radian*radian))
+t_hoh = 104.52 * degrees
+
+def test_flex(verbose=False):
+    s = MoleculeParser.read("../io/waterbox.gro", "../io/waterbox.grotop",
+                            {"GROMACS_PATH":"../io/gromacs"})
+
+    water = s.search("molidx 0")[0]
+
+    bonds = water.property("bond")
+    angles = water.property("angle")
+
+    assert_equal( bonds.nFunctions(), 2 )
+    assert_equal( angles.nFunctions(), 1 )
+
+    # Debug errors by getting the warnings from the parser...
+    #g = GroTop("../io/waterbox.grotop", {"GROMACS_PATH":"../io/gromacs"})
+    #
+    #for warning in g.warnings():
+    #    print(warning)
+
+    boh1 = bonds.potential( OW, HW1 )
+    boh2 = bonds.potential( OW, HW2 )
+
+    if verbose:
+        print("Bond OW-HW1 = %s" % boh1)
+        print("Bond OW-HW2 = %s" % boh2)
+
+    assert_equal(boh1, boh2)
+
+    r = Symbol("r")
+
+    amb_boh = AmberBond(boh1, r)
+
+    if verbose:
+        print("Amber Bond OW-HW1 = %s" % amb_boh)
+
+    assert_almost_equal( amb_boh.r0(), r_oh.value() )
+    assert_almost_equal( amb_boh.k(), k_oh.value() )
+
+    hoh = angles.potential(HW1, OW, HW2)
+
+    if verbose:
+        print("Angle HW1-OW-HW2 = %s" % hoh)
+
+    t = Symbol("theta")
+
+    amb_hoh = AmberAngle(hoh, t)
+
+    if verbose:
+        print("Amber Angle HW1-OW-HW2 = %s" % amb_hoh)
+
+    assert_almost_equal( amb_hoh.theta0(), t_hoh.value() )
+    assert_almost_equal( amb_hoh.k(), k_hoh.value() )        
+
+if __name__ == "__main__":
+    test_flex(True)
---
+++ unittests/SireVol/test_spacewrapper.py
@@ -0,0 +1,74 @@
+
+from Sire.IO import *
+from Sire.Move import *
+from Sire.System import *
+from Sire.MM import *
+from Sire.Mol import *
+from Sire.Maths import *
+from Sire.CAS import *
+from Sire.Units import *
+
+def test_wrapper(verbose=False):
+    s = MoleculeParser.read("../io/KCL-ok.crd", "../io/KCl-ok.top")
+
+    space = s.property("space")
+
+    all = s[MGName("all")]
+
+    ff = InterFF("cljff")
+    ff.setCLJFunction( CLJShiftFunction(7.5*angstrom) )
+    ff.add(all)
+
+    ff2 = InterFF("cljff2")
+    ff2.setCLJFunction( CLJShiftFunction(7*angstrom) )
+    ff2.add(all)
+
+    s.add(ff)
+    s.add(ff2)
+
+    s.setProperty("space", space)
+
+    slow = ff.components().total()
+    fast = ff2.components().total()
+
+    slow_s = Symbol("E_slow")
+    fast_s = Symbol("E_fast")
+
+    s.setComponent(slow_s, slow)
+    s.setComponent(fast_s, fast)
+    s.setComponent(s.totalComponent(), slow_s)
+
+    s.add( SpaceWrapper(Vector(0), all) )
+
+    move = RigidBodyMC(all)
+    vmove = VolumeMove(all)
+    moves = WeightedMoves()
+    moves.add(move, 100)
+    moves.add(vmove, 20)
+
+    mtsmc = MTSMC(moves, 100)
+    mtsmc.setFastEnergyComponent(fast_s)
+    mtsmc.setSlowEnergyComponent(slow_s)
+
+    moves = SameMoves(mtsmc)
+
+    if verbose:
+        print("Moving the molecules...")
+
+    for i in range(0,10):
+        if verbose:
+            print("Move %d" % (i+1))
+
+        s = moves.move(s, 1)
+
+        if verbose:
+            print(moves)
+
+        p = PDB2(s)
+        p.writeToFile("test%03d.pdb" % (i+1))
+
+    if verbose:
+        print("Moves complete")
+
+if __name__ == "__main__":
+    test_wrapper(True)

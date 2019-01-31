d5f666856042ef8021affeac88b3686d4b9c187c
Christopher Woods <chryswoods@gmail.com>
2019-01-31 19:27:07 +0000

    Fleshed out the test_unnamed.py script so that as well as testing the modification of unnamed molecules, it also tests that all of the (much more complex) information has been added correctly.

--- unittests/SireMol/test_unnamed.py
+++ unittests/SireMol/test_unnamed.py
@@ -7,12 +7,31 @@ def test_unnamed(verbose=False):
     na = Molecule()
     na = na.edit().add( CGName("1") ).add( AtomName("Na") ).molecule().commit()
 
-    na = na.edit().atom(AtomIdx(0)) \
-                  .setProperty("coordinates", Vector(0,0,0)) \
+    na = na.edit().atom(AtomIdx(0)).renumber(AtomNum(5)) \
+                  .setProperty("coordinates", Vector(1,2,3)) \
                   .molecule() \
+           .add( ResName("ION") ).renumber( ResNum(42) ).molecule() \
+                  .atom(AtomIdx(0)).reparent(ResName("ION")).molecule() \
                   .commit()
 
+    if verbose:
+        print(na)
+
+        for atom in na.atoms():
+            print(atom, atom.property("coordinates"))
+
+        for residue in na.residues():
+            print(residue)
+
     assert_equal(na.nAtoms(), 1)
+    assert(na.number().value() != 0)
+
+    assert_equal(na.atom(AtomIdx(0)).property("coordinates"), Vector(1,2,3))
+    assert_equal(na.atom(AtomIdx(0)).name().value(), "Na")
+    assert_equal(na.atom(AtomIdx(0)).number().value(), 5)
+    assert_equal(na.atom(AtomIdx(0)).residue(), na.residue(ResIdx(0)))
+    assert_equal(na.residue(ResIdx(0)).name().value(), "ION")
+    assert_equal(na.residue(ResIdx(0)).number().value(), 42)
 
 if __name__ == "__main__":
     test_unnamed(True)
